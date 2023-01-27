73afe3be1ffbcd18a361e89715db769a859e3622
Florian Pix <florian.pix.97@outlook.com>
2023-01-27 22:59:21 +0100

    fixed bug in spotService.updateSpot and .getSpot: wrong return;

--- src/flutterApp/lib/services/spot_service.dart
+++ src/flutterApp/lib/services/spot_service.dart
@@ -35,7 +35,7 @@ class SpotService {
 
     if (response.statusCode == 200) {
       // If the server did return a 200 OK response, then parse the JSON.
-      return response.data;
+      return Spot.fromJson(response.data);
     } else {
       // If the server did not return a 200 OK response,
       // then throw an exception.
@@ -95,7 +95,7 @@ class SpotService {
 
     if (response.statusCode == 200) {
       // If the server did return a 200 OK response, then parse the JSON.
-      return response.data;
+      return Spot.fromJson(response.data);
     } else {
       // If the server did not return a 200 OK response,
       // then throw an exception.
