4a18c5aafa2396e65c0bb39ee87b13d05c0c2436
florianpix <florian.pix.97@outlook.com>
2023-05-07 19:36:41 +0200

    added simple heatmap calendar; closes #90;

--- src/flutter_app/lib/pages/statistic_page.dart
+++ src/flutter_app/lib/pages/statistic_page.dart
@@ -1,22 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:overlay_support/overlay_support.dart';
+import 'package:flutter_heatmap_calendar/flutter_heatmap_calendar.dart';
 
-class StatisticPage extends StatelessWidget {
+import '../interfaces/ascent/ascent.dart';
+import '../services/ascent_service.dart';
+
+class StatisticPage extends StatefulWidget {
   const StatisticPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() => StatisticPageState();
+}
+
+class StatisticPageState extends State<StatisticPage> {
+  final AscentService ascentService = AscentService();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            showSimpleNotification(
-              const Text("Statistics page is not implemented yet"),
-              background: Colors.red,
-              duration: const Duration(seconds: 4)
-            );
+        child: FutureBuilder<List<Ascent>>(
+          future: ascentService.getAscents(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              List<Ascent> ascents = snapshot.data!;
+              Map<DateTime, int> datasets = <DateTime, int>{};
+              for (var ascent in ascents) {
+                DateTime date = DateTime.parse(ascent.date);
+                if (datasets.keys.contains(date)){
+                  datasets[date] = (datasets[date]! + 1);
+                } else {
+                  datasets[date] = 1;
+                }
+              }
+              return HeatMapCalendar(
+                defaultColor: Colors.white,
+                flexible: true,
+                colorMode: ColorMode.opacity,
+                datasets: datasets,
+                colorsets: const {
+                  1: Colors.red,
+                  3: Colors.orange,
+                  5: Colors.yellow,
+                  7: Colors.green,
+                  9: Colors.blue,
+                  11: Colors.indigo,
+                  13: Colors.purple,
+                },
+                onClick: (value) {
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(value.toString())));
+                },
+              );
+            } else {
+              return const CircularProgressIndicator();
+            }
           },
-          child: const Text("Statistics"),
         ),
       ),
     );
