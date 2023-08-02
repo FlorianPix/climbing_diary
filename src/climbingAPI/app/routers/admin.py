77da44175559158bc0ed24c7a39599999ca52382
florianpix <florian.pix.97@outlook.com>
2023-08-02 18:41:37 +0200

    added migration endpoint to add missing updated field to existing data;

--- src/climbingAPI/app/routers/admin.py
+++ src/climbingAPI/app/routers/admin.py
@@ -1,3 +1,5 @@
+import datetime
+
 from fastapi import APIRouter, Depends, Security
 from fastapi_auth0 import Auth0User
 
@@ -5,17 +7,21 @@ from app.core.db import get_db
 from app.core.auth import auth
 
 router = APIRouter()
+collection_names = ["trip", "spot", "single_pitch_route", "multi_pitch_route", "pitch", "ascent"]
+
+
+@router.delete('/migrate', description="Migrate db from v0.0.4-alpha to v0.1.0-alpha", dependencies=[Depends(auth.implicit_scheme)])
+async def migrate_all(user: Auth0User = Security(auth.get_user, scopes=["write:diary"])):
+    db = await get_db()
+    for collection_name in collection_names:
+        await db[collection_name].update_many({"user_id": user.id}, {"$set": {"updated": datetime.datetime.now()}})
 
 
 @router.delete('/', description="Delete everything from this user", dependencies=[Depends(auth.implicit_scheme)])
 async def delete_all(user: Auth0User = Security(auth.get_user, scopes=["write:diary"])):
     db = await get_db()
-    await db["trip"].delete_many({"user_id": user.id})
-    await db["spot"].delete_many({"user_id": user.id})
-    await db["single_pitch_route"].delete_many({"user_id": user.id})
-    await db["multi_pitch_route"].delete_many({"user_id": user.id})
-    await db["pitch"].delete_many({"user_id": user.id})
-    await db["ascent"].delete_many({"user_id": user.id})
+    for collection_name in collection_names:
+        await db[collection_name].delete_many({"user_id": user.id})
 
 
 @router.delete('/trips', description="Delete all trips from all users", dependencies=[Depends(auth.implicit_scheme)])
