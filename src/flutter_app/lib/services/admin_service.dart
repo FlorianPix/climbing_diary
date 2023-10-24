92f08b8a981fef357f2e0c2f583345be004dbfcb
florianpix <florian.pix.97@outlook.com>
2023-10-24 18:42:21 +0200

    wip migration;

--- src/flutter_app/lib/services/admin_service.dart
+++ src/flutter_app/lib/services/admin_service.dart
@@ -1,6 +1,10 @@
+import 'dart:typed_data';
+
 import 'package:climbing_diary/components/common/my_notifications.dart';
+import 'package:climbing_diary/interfaces/media/media.dart';
 import 'package:climbing_diary/services/cache_service.dart';
 import 'package:climbing_diary/services/error_service.dart';
+import 'package:climbing_diary/services/media_service.dart';
 import '../config/environment.dart';
 import 'package:dio/dio.dart';
 
@@ -14,6 +18,7 @@ class AdminService {
   final sharedPrefLocator = getIt.get<SharedPreferenceHelper>();
   final String climbingApiHost = Environment().config.climbingApiHost;
   final String mediaApiHost = Environment().config.mediaApiHost;
+  MediaService mediaService = MediaService();
 
   Future<void> deleteAll() async {
     try {
@@ -39,11 +44,18 @@ class AdminService {
         final idUrl = mediumResponse.data!;
         String url = idUrl['url'];
         // download image
-        var uri = Uri.https(url);
-        var response = await http.get(uri);
-        print('Response status: ${response.statusCode}');
-        // convert to UInt8List
+        var uri = Uri.parse(url);
+        http.Response response = await http.get(uri);
+        Uint8List image = response.bodyBytes;
         // create medium
+        Media createMedium = Media(
+          id: medium['id'],
+          userId: medium['user_id'],
+          title: medium['title'],
+          createdAt: DateTime.parse(medium['created_at']).toIso8601String(),
+          image: image
+        );
+        await mediaService.createMedium(createMedium);
       }
     } catch (e) {
       ErrorService.handleConnectionErrors(e);
